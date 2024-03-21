3efa58b60b91becd0dd8c1ba46add37177a1f880
Argyris <arkatsy@gmail.com>
2024-03-21 23:39:33 +0200

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="min-h-16">Settings</div>
+      <div>Editor</div>
+      <div>Preview</div>
+    </div>
+  );
+}
+
+export default App;
